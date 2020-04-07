29a24d5f380b6c4c17c6f7b2d85371bd23638b88
tol2000 <tolicheg@gmail.com>
2020-04-07 21:50:02 +0300

    export_import_almost_work

--- project/shells.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-konsole --separate --new-tab -e "docker exec -it $(docker ps -a -q -f name=sdc1) bash"&
-konsole --separate --new-tab -e "docker exec -it $(docker ps -a -q -f name=quickstart.cloudera) bash"&
